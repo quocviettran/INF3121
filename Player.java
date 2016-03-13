e1b718bc43a9479c50aba8bbf2275753862dd810
quocviettran <qvtran@uio.no>
2016-03-13 12:10:18 +0100

    Revert "INF3121 PA1"
    This reverts commit ab8cee88a658f6de3e671e4ca56fdba5f36b5049.

--- Player.java
+++
@@ -1,9 +0,0 @@
-package telerik;
-public class Player {		//This class is only for the highscore board, where the programs enter the amount of moves made by the player after you've won.
-	String name;			
-	int movesCount;			
-	public Player(String name, int movesCount){	
-		this.name = name;
-		this.movesCount = movesCount;
-	}
-}
